ee517adbb721a887c57e9f3da1f131a9f46565e6
Caleb Salyards <34790708+Salsonn@users.noreply.github.com>
2021-12-01 12:53:01 -0700

    Update grapes_of_wrath.py

--- docs/res/grapes_of_wrath.py
+++ docs/res/grapes_of_wrath.py
@@ -2,7 +2,7 @@ class Queue:
     def __init__(self):
         self.queue = []
 
-    def enqueue(self, addition)
+    def enqueue(self, addition):
     # This function adds the contents of 'addition' to the class
         self.queue.append(addition)
         return True
